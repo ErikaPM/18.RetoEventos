2235d5073c749ff01ca83509d361a79f4ab5b5f1
Erika <53281048+ErikaPM@users.noreply.github.com>
2019-10-30 10:03:30 -0500

    Add files via upload

---
+++ 18. RetoEventos/appreto.js
@@ -0,0 +1,8 @@
+// const caja = document.querySelectorAll("#caja");
+// const botonAgregar = document.querySelectorAll("#agregar");
+// const botonBorrar = document.querySelectorAll("#borrar");
+
+const nuevaCaja = document.createElement("div");
+const padre = document.createElement("padre");
+// const contenido = document.createTextNode("1");
+nuevaCaja.appendChild(padre);
